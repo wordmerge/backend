372498e750894bd65b358646fd5dedcdb6364d0e
Alastair Paragas <alastairparagas@gmail.com>
2016-09-24 10:49:11 -0400

    Auth Routes

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const App = require('express')(),
       AuthController = require('./app/controllers/AuthController'),
       RoomController = require('./app/controllers/RoomController');
 
+App.post('/auth/login', AuthController.login);
+App.post('/auth/signup', AuthController.signup);
+
 App.listen(process.env.PORT, () => {
   console.log("Listening to Port:" + process.env.PORT);
 });
