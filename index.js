b109efcff6efb43fae2ffda5d92655b69da3a871
Alastair Paragas <alastairparagas@gmail.com>
2016-09-24 08:33:01 -0400

    Fix exposed files

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ require('dotenv').config({
   path: '../.env'
 });
 
+App.post('/auth/login', AuthController.login());
+App.post('/auth/signup', AuthController.signup());
 
+App.post('/room/create_specific', RoomController.create_specific());
 
 App.listen(process.env.PORT, () => {
   console.log("Listening to Port:" + process.env.PORT);
