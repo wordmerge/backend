dcbb2d0be8d4e04726898f27950bded82c2b0b40
Alastair Paragas <alastairparagas@gmail.com>
2016-09-24 08:20:46 -0400

    Fix env and scaffold controllers

--- app/index.js
+++ app/index.js
@@ -1,1 +1,13 @@
+const App = require('express')(),
+      AuthController = require('./controllers/AuthController'),
+      RoomController = require('./controllers/RoomController');
 
+require('dotenv').config({
+  path: '../.env'
+});
+
+
+
+App.listen(process.env.PORT, () => {
+  console.log("Listening to Port:" + process.env.PORT);
+});
