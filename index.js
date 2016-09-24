49a32fc3879fc78ea786f5d21ee6b2fb71a237c4
Alastair Paragas <alastairparagas@gmail.com>
2016-09-24 09:18:32 -0400

    Fixed bug with Docker volumes/paths

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
+
 const App = require('express')(),
       AuthController = require('./app/controllers/AuthController'),
       RoomController = require('./app/controllers/RoomController');
-
-require('dotenv').config();
 
 App.listen(process.env.PORT, () => {
   console.log("Listening to Port:" + process.env.PORT);
