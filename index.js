1206322598379ddc588649c3afb62c79b87fcb5f
JGMorgan <josegm11194@gmail.com>
2016-09-25 08:16:00 -0400

    BugFixes for Beta

--- index.js
+++ index.js
@@ -51,34 +51,77 @@ function sleep(time, callback) {
     callback();
 }
 
-<<<<<<< HEAD
 /*sleep(20000, () => {
     AuthController.signup({body: {email: 'test1@gmail.com', username: 'bill', password: '1234', image_url: ''}}, {});
     AuthController.signup({body: {email: 'test@gmail.com', username: 'frank', password: '1234', image_url: ''}}, {});
 });*/
-=======
 sleep(20000, () => {
     AuthController.signup({body: {
-      email: 'test1@gmail.com', 
-      username: 'bill', 
-      password: '1234', 
+      email: 'test1@gmail.com',
+      username: 'bill',
+      password: '1234',
       image_url: ''}}, {
     });
     AuthController.signup({body: {
-      email: 'test@gmail.com', 
-      username: 'frank', 
-      password: '1234', 
+      email: 'test@gmail.com',
+      username: 'frank',
+      password: '1234',
       image_url: ''}}, {
     });
 });
->>>>>>> d343617d1b4cadf04bdd72260cd651789425fb71
 
 const Http = require('http'),
-      Server = Http.createServer(App), 
+      Server = Http.createServer(App),
       SocketIO = require('socket.io')(Server);
 
-SocketIO.on("connection",
-            (socket) => RoomsManager.addSocket(socket));
+//SocketIO.on("connection",
+            //(socket) => RoomsManager.addSocket(socket));
+var players = []
+var i = 0;
+SocketIO.on("connection", (socket) => {
+    /*if(i == 0) {
+        players[0] = socket;
+        players[0].on("user_typing", (data) => {
+            players[1].emit("user_typed")
+        })
+        players[0].on("say_word", (data) => {
+            players[other(1)].emit("said_word", {word: data.word});
+        })
+    } else {
+        players[1] = socket;
+        players[1].on("user_typing", (data) => {
+            players[other(0)].emit("user_typed")
+        })
+        players[1].on("say_word", (data) => {
+            players[other(0)].emit("said_word", {word: data.word});
+        })
+    }*/
+    /*players[i] = socket;
+    players[i].on("user_typing", (data) => {
+        players[data.id].emit("user_typed", {id: data.id})
+    })
+    players[i].on("say_word", (data) => {
+        players[data.id].emit("said_word", {word: data.word, id: data.id});
+    })*/
+    socket.on("in", (data) => {
+        players[data.id] = socket;
+        socket.other = other(data.id);
+    })
+    socket.on("user_typing", (data) => {
+        players[socket.other].emit("user_typed", {id: data.id})
+    })
+    socket.on("say_word", (data) => {
+        players[socket.other].emit("said_word", {id: data.id})
+    })
+    i = other(i);
+})
+
+function other(i) {
+    if(i == 0)
+        return 1;
+    else
+        return 0;
+}
 
 Server.listen(process.env.PORT, () => {
   console.log("Listening to Port:" + process.env.PORT);
